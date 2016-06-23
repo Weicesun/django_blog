a37aa1758d53b033f9824ad8758ecfe4f714d301
vagrant <vagrant@precise64.(none)>
2016-06-23 16:32:15 +0000

    follow to css lesson

---
+++ blog/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+# Create your views here.
+def post_list(request):
+    post = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts':post})
