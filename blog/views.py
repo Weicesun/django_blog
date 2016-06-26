488c038ba0c196898c3f090acd9828b109179f86
Weicesun <kevinsun181@gmail.com>
2016-06-26 14:52:41 +0000

    add post page

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 # Create your views here.
 def post_list(request):
     post = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -8,3 +9,17 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html',{'post':post})
+def post_view(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html',{'form':form})
+def post_new(request):
+    form = PostForm(request.POST)
+    if form.is_valid():
+	post = form.save(commit = False)
+	post.author = request.user
+	post.published_date = timezone.now()
+	post.save()
+	return redirect('post_detail', pk=post.pk)
+    else:
+	form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form':form})
